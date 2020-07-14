843cffb301de11e3fea54414b152693e0edb23ed
Abdul Rafay <arafay@nisum.com>
2020-07-14 17:45:07 +0500

    Updates

---
+++ precache-manifest.0064183e519bcc5c240b958fc0fbdb82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48f982e60a0d3a4f9d1295941f49071",
+    "url": "/graphql-mern-event-booking/index.html"
+  },
+  {
+    "revision": "ef54e7c021ba2a8fa0fc",
+    "url": "/graphql-mern-event-booking/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "d78fe6691954b0f3d777",
+    "url": "/graphql-mern-event-booking/static/css/main.74bea252.chunk.css"
+  },
+  {
+    "revision": "ef54e7c021ba2a8fa0fc",
+    "url": "/graphql-mern-event-booking/static/js/2.bbd283a3.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/graphql-mern-event-booking/static/js/2.bbd283a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78fe6691954b0f3d777",
+    "url": "/graphql-mern-event-booking/static/js/main.8ac6346e.chunk.js"
+  },
+  {
+    "revision": "bdcb6707439d6ca454b5",
+    "url": "/graphql-mern-event-booking/static/js/runtime-main.c28a3362.js"
+  }
+]);
