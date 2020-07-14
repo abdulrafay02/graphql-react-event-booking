75f16212d135aaf1663014a3d1fa042bef96be21
Abdul Rafay <arafay@nisum.com>
2020-07-14 18:04:58 +0500

    api url fix

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = window.BASE_URL || "http://localhost:4000/graphql";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000/graphql"
+    : "https://graphql-mern-event-booking-api.herokuapp.com/graphql";
