9410bcee48ff481bfa694e3561783a283c717d7d
Abdul Rafay <arafay@nisum.com>
2020-07-14 18:19:27 +0500

    minor fixes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4000/graphql"
-    : "https://graphql-mern-event-booking-api.herokuapp.com/graphql";
+export const BASE_URL = process.env.REACT_APP_API_URI;
